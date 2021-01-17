16fc0db8a6bb9124b706c5009d86abe7ab0ea8f3
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-01-17 17:51:30 +0700

    5:51PM Jan-17-2021: Refractor & Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
-
-const onClick = () => {
-  console.log("OKOK");
-}
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from "react-router-dom";
+import Home from "./Home";
 
 const App = () => {
   return (
-    <div style={{display: "flex", justifyContent: "center", fontFamily: "Roboto"}}>
-      <div style={{flexDirection: "column", overflowWrap: "break-word", width: 1100, textAlign: "justify"}}>
-        <img src="safeq.png" alt="OKOK" style={{width: 300, height: 300}}/>
-        <h1 className="typewriter"><span style={{color: "#006400"}}>SafeQ Inc.</span> - the best solution for queueing during Covid-19</h1>
-        <p style={{fontSize: 22, fontWeight: "bold"}}>Mission:</p>
-        <p style={{fontSize: 22}}>
-          Realizing that social distancing has been one of the hottest topic in the past 2 years, my
-          team has developed <span style={{color: "#006400", fontWeight: "bold"}}>SafeQ</span>. 
-          This solution enables people to arrive just-in-time for appointments, 
-          which significantly minimizes crowdedness of any office. 
-          SafeQ guarantees safe queueing environments! 
-          Last but not least, thank you so much for using our product.
-          Your support is crucial to social goods and to our further development. 
-          Feedbacks can be sent via <a href="mailto:safeqinc@gmail.com">safeqinc@gmail.com</a>.
-        </p>
-        <div style={{display: "flex", justifyContent: "center"}}>
-          <button className="button" onClick={onClick} style={{}}>
-            <img src="attend.png" alt="attend.png" style={{width: 75, height: 75}}/>
-            <h2>Attend</h2>
-          </button>
-          <button className="button">
-            <img src="schedule.png" alt="schedule.png" style={{width: 75, height: 75}}/>
-            <h2>Schedule</h2>
-          </button>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/home" component={Home}></Route>
+    </Router>
   )
 };
 
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
+const Home = () => {
+  return (
+    <Router>
+      <div style={{display: "flex", justifyContent: "center", fontFamily: "Roboto"}}>
+        <div style={{flexDirection: "column", overflowWrap: "break-word", width: 1100, textAlign: "justify"}}>
+          <img src="safeq.png" alt="OKOK" style={{width: 300, height: 300}}/>
+          <h1 className="typewriter"><span style={{color: "#006400"}}>SafeQ Inc.</span> - the best solution for queueing during Covid-19</h1>
+          <p style={{fontSize: 22, fontWeight: "bold"}}>Mission:</p>
+          <p style={{fontSize: 22}}>
+            Realizing that social distancing has been one of the hottest topic in the past 2 years, my
+            team has developed <span style={{color: "#006400", fontWeight: "bold"}}>SafeQ</span>.
+            This solution enables people to arrive just-in-time for appointments, 
+            which significantly minimizes crowdedness of any office. 
+            SafeQ guarantees safe queueing environments! 
+            Last but not least, thank you so much for using our product.
+            Your support is crucial to social goods and to our further development. 
+            Feedbacks can be sent via <a href="mailto:safeqinc@gmail.com">safeqinc@gmail.com</a>.
+            We would love to know what you think about SafeQ.
+          </p>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            <Link to="/attend">
+              <button className="button">
+                <img src="attend.png" alt="attend.png" style={{width: 75, height: 75}}/>
+                <h2>Attend</h2>
+              </button>
+            </Link>
+            <Link to="/schedule">
+              <button className="button">
+                <img src="schedule.png" alt="schedule.png" style={{width: 75, height: 75}}/>
+                <h2>Schedule</h2>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+};
+
+export default Home;
